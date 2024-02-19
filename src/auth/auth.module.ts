e2414f678d8cd05353068e50c074a274f6e4753f
Usman Siddiqui <siddiquiusman328@gmail.com>
2024-02-19 14:16:38 +0500

    ...

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -1,18 +1,25 @@
-import { Module } from '@nestjs/common';
+import { Module, NestModule, MiddlewareConsumer } from '@nestjs/common';
+import { CheckEmailMiddleware } from 'src/middlewares/CheckEmailAvailablity.middleware';
 import { AuthController } from './auth.controller';
 import { AuthService } from './auth.service';
 import { UsersModule } from 'src/users/users.module';
 import { JwtModule } from '@nestjs/jwt';
 
 @Module({
-    imports:[UsersModule,
-    JwtModule.register({
-      global: true,
-      secret: process.env.JWT_SECRET || "dajsdkh823948dk",
-      signOptions: { expiresIn: '60s' },
-    }),
+    imports: [UsersModule,
+        JwtModule.register({
+            global: true,
+            secret: process.env.JWT_SECRET || "dajsdkh823948dk",
+            signOptions: { expiresIn: '60s' },
+        }),
     ],
-  controllers: [AuthController],
-  providers: [AuthService]
+    controllers: [AuthController],
+    providers: [AuthService]
 })
-export class AuthModule {}
+export class AuthModule implements NestModule {
+    configure(consumer: MiddlewareConsumer) {
+        consumer
+            .apply(CheckEmailMiddleware)
+            .forRoutes('auth/signup');
+    }
+}
